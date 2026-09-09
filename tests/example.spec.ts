1c4b7f08ef6bca4dda2198e61d3978cb5ebf7e52
vishnujadhav192 <vishnujadhav192@gmail.com>
2026-09-09 10:55:02 +0530

    Changes

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,10 +4,23 @@ import * as dotenv from 'dotenv';
 // Load variables from .env file
 dotenv.config();
 
-test('Greetings', async () => {
-  const userName = process.env.USER_NAME;
-  const userMessage = process.env.USER_MESSAGE;
+test('test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('#user-name').click()
 
-  console.log(`Hello ${userName} !!!! ${userMessage}`);
+  let username = process.env.USERNAME1;
+  let password = process.env.PASSWORD1;
 
+  if (username !== undefined) {
+    await page.locator('[data-test="username"]').fill(username);
+  }
+  await page.locator('#password').click();
+
+  if (password !== undefined) {
+    await page.locator('[data-test="password"]').fill(password);
+  }
+  
+  await page.locator('[data-test="login-button"]').click();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.locator('[data-test="logout-sidebar-link"]').click();
 });
